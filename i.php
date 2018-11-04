80cbddc27ebd17bf514362e4d086eccffb4cabb1
EotL Webmaster <webmaster@eotl.org>
2018-11-04 04:30:12 -0600

    Initial commit

---
+++ i.php
@@ -0,0 +1,312 @@
+<?php
+// echo "This will work: {$a[3][bar]}";  (putting inside a string)
+// echo $banner[ "eotl" ][ "file" ];    will output "eotl_banner4.gif"
+$banner = array(
+  1 => array(
+    "alt"  => "Eight Ball Bar and Grill",
+    "file" => "8ball.gif",
+    "url"  => "",
+    "text" => "Voted \"Best Cajun Food in Eternal City!\""
+  ),
+  2 => array(
+    "alt"  => "Paranoid Clothing and Apparel",
+    "file" => "paranoid.gif",
+    "url"  => "",
+    "text" => "Definitely the place to shop, because everyone is out to get you."
+  ),
+  3 => array(
+    "alt"  => "(It's Purple!)",
+    "file" => "purple.gif",
+    "url"  => "http://www.wenet.net/~purple/purple.html",
+    "text" => "(This space intentionally left Purple)."
+  ),
+  4 => array(
+    "alt"  => "Dear Reverend - Advice from the mallet",
+    "file" => "dearreverend.gif",
+    "url"  => "http://www.eotl.org/dearreverend/",
+    "text" => "You know you're burning to ask."
+  ),
+  5 => array(
+    "alt"  => "Berzerker Guild",
+    "file" => "zerkers.jpg",
+    "url"  => "http://www.eotl.org/guilds/berserker.html",
+    "text" => ""
+  ),
+  6 => array(
+    "alt"  => "Crimson City Wedding Chapel",
+    "file" => "wedbanner3.jpg",
+    "url"  => "",
+    "text" => "For when you're feeling either lonely or hungry."
+  ),
+  7 => array(
+    "alt"  => "Pacifist Guild",
+    "file" => "pacifist.jpg",
+    "url"  => "http://www.eotl.org/guilds/pacifist.html",
+    "text" => ""
+  )
+);
+
+echo '<?xml version="1.0" encoding="UTF-8" ?>';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+  <title>End of the Line LP Mud</title>
+  <meta name="keywords" content="End of the Line EotL LPmud LP Mud adventure game" />
+  <meta name="description" content="End of the Line, more commonly known as EotL, is a text-based roleplaying game in which you adventure in a myriad of landscapes and realms.  It is an LP-style of MUD that was first opened to the public in 1989." />
+  <link rel="STYLESHEET" type="text/css" href="/css/eotl_0.css" />
+</head>
+
+<body bgcolor="#000000" link="#996699" vlink="#9966CC">
+
+<table border="1" cellpadding="0" cellspacing="0">
+  <tr>
+  <td rowspan="20"><img src="/img/clr.gif" alt="" width="1" height="1" hspace="5" /></td>
+    <td bgcolor="#000000" width="582" nowrap="nowrap" align="center"><span style="color: #FFFFFF;"
+    ><img src="/img/title.gif"
+    alt="End of the Line LP Mud - 171.64.14.16 2010" width="582" height="90" /></span><br />
+    <table border="0" cellpadding="0" cellspacing="0"><tr><td height="1"></td></tr></table></td>
+  </tr>
+
+  <tr>
+    <td bgcolor="#660066"><table border="0" cellpadding="0" cellspacing="0"><tr><td height="1"></td></tr></table></td>
+  </tr>
+
+  <tr>
+    <td bgcolor="#000000" height="10" align="center">
+
+<a class="menu" href="/gameinfo.html" target="_top"
+><img src="/img/point.gif" alt="" width="12" height="8" border="0" /></a
+><a class="menu" href="/gameinfo.html" target="_top">Game&#160;Info</a>
+
+<img src="/img/clr.gif" alt="" width="1" height="1" hspace="20" />
+
+<a class="menu" href="/players.html" target="_top"
+><img src="/img/point.gif" alt="" width="12" height="8" border="0" /></a
+><a class="menu" href="/players.html" target="_top">Players</a>
+
+<img src="/img/clr.gif" alt="" width="1" height="1" hspace="20" />
+
+<a class="menu" href="/fun.html" target="_top"
+><img src="/img/point.gif" alt="" width="12" height="8" border="0" /></a
+><a class="menu" href="/fun.html" target="_top">Fun</a>
+
+<img src="/img/clr.gif" alt="" width="1" height="1" hspace="20" />
+
+<a class="menu" href="/mud_info.html" target="_top"
+><img src="/img/point.gif" alt="" width="12" height="8" border="0" /></a
+><a class="menu" href="/mud_info.html" target="_top">MUD&#160;Links</a>
+
+<img src="/img/clr.gif" alt="" width="1" height="1" hspace="20" />
+
+<a class="menu" href="/guestbook/" target="_top"
+><img src="/img/point.gif" alt="" width="12" height="8" border="0" /></a
+><a class="menu" href="/guestbook/" target="_top">Guestbook</a><br />
+
+    </td>
+  </tr>
+
+  <tr>
+    <td bgcolor="#660066"><table border="0" cellpadding="0" cellspacing="0"><tr><td height="1"></td></tr></table></td>
+  </tr>
+
+  <tr>
+    <td bgcolor="#000000" width="582" height="10" align="center">
+
+<img src="/img/clr.gif" alt="" width="1" height="1" vspace="7" /><br />
+
+<?php
+# randomize these banners!!!
+$foo = sizeof( $banner );
+// seed with microseconds since last "whole" second
+mt_srand( ( double ) microtime() * 1000000 );
+$bnr = mt_rand( 1, $foo );
+$alt = $banner[ $bnr ][ "alt" ];
+if( $alt != 'FLASH' ) {
+  $url = $banner[ $bnr ][ "url" ];
+  if( $url != "" ) {
+    printf( "<a href=\"%s\">", $url );
+  }
+  printf( "<img src=\"/img/banners/%s\" alt=\"AD: %s\" width=\"468\" height=\"60\" />",
+    $banner[ $bnr ][ "file" ], $alt );
+  if( $url != "" ) {
+    echo "</a>";
+  }
+} else { // Flash movie
+  echo $banner[ $bnr ][ "code" ];
+}
+echo "<br />\n";
+$text = $banner[ $bnr ][ "text" ];
+if( $text != "" ) {
+?>
+  <span style="color: #FFFFFF; font-family: verdana, arial, helvetica, geneva, sans-serif; font-size: 8pt;"
+<?php
+  printf( "  ><font color=\"#FFFFFF\" face=\"verdana,arial,helvetica\">%s</font></span>\n", $text );
+}
+?>
+
+<br />
+    </td>
+  </tr>
+
+  <tr>
+    <td bgcolor="#000000"><table border="0" cellpadding="0" cellspacing="0"><tr><td height="9"></td></tr></table></td>
+  </tr>
+</table>
+
+<table border="1" cellpadding="1" cellspacing="0">
+<tr>
+  <td rowspan="20"><img src="/img/clr.gif" alt="" width="1" height="1" hspace="3" /></td>
+  <td bgcolor="#660066">
+
+  <table border="1" cellpadding="0" cellspacing="0">
+  <tr>
+    <td bgcolor="#FFFFFF" width="15" height="15" valign="top"><img src="/img/clr.gif" alt="" width="1" height="1"
+      hspace="7" vspace="7" /></td>
+    <td bgcolor="#FFFFFF" width="410" height="15"><img src="/img/clr.gif" alt="" width="1" height="1" hspace="204" /></td>
+    <td bgcolor="#FFFFFF" width="15" height="15"><img src="/img/clr.gif" alt="" width="1" height="1" hspace="7" /></td>
+    <td bgcolor="#000000" width="140" height="15"><img src="/img/clr.gif" alt="" width="1" height="1" /></td>
+  </tr>
+
+  <tr>
+    <td bgcolor="#FFFFFF" width="15"><img src="/img/clr.gif" alt="" width="1" height="1" hspace="7" /></td>
+
+<!--  BEGIN MAIN SECTION  -->
+    <td bgcolor="#FFFFFF" width="410" valign="top">
+
+<p align="center" class="bignpurple"><strong>
+One of the oldest LP Muds in existence &#151; since 1989!</strong></p>
+
+<p>
+<img src="/img/clr.gif" alt="" width="1" height="1" hspace="10" />
+<strong><font size="+3"><span class="bignpurple">E</span></font>nd of the Line</strong>, more
+commonly known as <strong>EotL</strong>, is a text-based
+roleplaying game in which you adventure in a myriad of
+landscapes and realms.
+You can interact with many other players, many of whom connect
+from locations such as Hong Kong, Chile, Australia, Canada,
+Russia, England, and the United States (the San Francisco
+Bay area in particular).
+
+<br />
+
+<img src="/img/clr.gif" alt="" width="1" height="1" hspace="10" />
+<font size="+3"><span class="bignpurple">E</span></font>otL is part of the LP-family of MUDs,
+which involves roleplaying and adventuring,
+and which is primarily combat-oriented
+(although there are many social players as well).
+</p>
+
+<table border="0" cellpadding="0" cellspacing="0">
+<tr>
+  <td colspan="3" class="ss10">
+<strong>Game Specs:</strong><br />
+<img src="/img/sword_tiny.gif" alt="*" width="21" height="20" /> Many <a href="/races/">races</a> available, from elves to androids to squids<br />
+<img src="/img/sword_tiny.gif" alt="*" width="21" height="20" /> Many <a href="/guilds/">guilds</a>, each with their own set of skills to train in<br />
+<img src="/img/sword_tiny.gif" alt="*" width="21" height="20" /> No need for character pre-approval<br />
+<img src="/img/sword_tiny.gif" alt="*" width="21" height="20" /> Internal Mail and News system<br />
+<img src="/img/sword_tiny.gif" alt="*" width="21" height="20" /> Many chat channels, both public and private<br />
+  </td>
+</tr>
+
+<tr>
+  <td valign="top" width="180" class="ss10">
+<img src="/img/sword_tiny.gif" alt="*" width="21" height="20" /> Ansi color option<br />
+<img src="/img/sword_tiny.gif" alt="*" width="21" height="20" /> Customizable prompt<br />
+  </td>
+  <td>&#160;&#160;&#160;&#160;&#160;</td>
+  <td valign="top" width="210" class="ss10">
+<img src="/img/sword_tiny.gif" alt="*" width="21" height="20" /> Unrestricted player-killing<br />
+<img src="/img/sword_tiny.gif" alt="*" width="21" height="20" /> Equipment storage<br />
+  </td>
+</tr>
+</table>
+
+<br />
+
+<p class="ss8"><font size="-2">
+Last updated: November 1, 2000<br />
+Please send your comments and questions to:
+<a href="mailto:webmaster@eotl.org">webmaster@eotl.org</a><br />
+Anonymous email without a valid return address will go directly to /dev/null.</font></p>
+
+    </td>
+<!--  END MAIN SECTION  -->
+
+    <td bgcolor="#FFFFFF" width="14"><img src="/img/clr.gif" alt="" width="1" height="1" hspace="6" /></td>
+
+    <td align="left" bgcolor="#000000" valign="top" width="140">
+
+<p align="center" class="ss8w"><a href="http://www.topmudsites.com/cgi-bin/topmuds/rankem.cgi?id=eotl"><img src="/img/vote_mud.gif" border="0" width="88" height="31" alt="Vote Here" /></a><br />
+Please vote only once per 12-hour period.</p>
+
+<h4 align="center" class="bignwhite"><font color="#FFFFFF">Quick News</font></h4>
+
+<table border="1" cellpadding="5" cellspacing="0"><tr><td bgcolor="#000000">
+
+<p class="ss8w"><font color="#FFFFFF" face="verdana,arial,helvetica,geneva" size="1">
+10/21/00: We have a new <a href="/guestbook/">guestbook</a>.
+Sign it, or else.</font></p>
+
+<p class="ss8w"><font color="#FFFFFF" face="verdana,arial,helvetica,geneva" size="1">
+10/00: Undeniable proof that Hannibal is
+<a href="http://www.eotl.org/photos/immortals/hannibal/1337.jpg">1337</a>.</font></p>
+
+<p class="ss8w"><font color="#FFFFFF" face="verdana,arial,helvetica,geneva" size="1">
+8/31/00: EotL driver update: LDMud 3.2.9.</font></p>
+
+<p class="ss8w"><font color="#FFFFFF" face="verdana,arial,helvetica,geneva" size="1">
+10/30/99: Dear Reverend is back!
+Submit your personal question on our
+<a href="/dearreverend/">online form</a>!</font></p>
+
+<p class="ss8w"><font color="#FFFFFF" face="verdana,arial,helvetica,geneva" size="1">
+EotL was <a href="/misc/mudconnect.html"><font color="#AD73A8">The Mud
+Connector</font></a>'s March&#160;1998 Mud of the Month!</font></p>
+
+</td></tr></table>
+
+<img src="/img/clr.gif" alt="" width="1" height="1" vspace="7" /><br />
+
+<div align="center">
+<table border="0" cellpadding="10" cellspacing="0"><tr>
+  <td align="center" bgcolor="#000000">
+
+  <table border="1" cellpadding="10" cellspacing="0"><tr>
+    <td align="center" bgcolor="#AD73A8">
+<p><img src="/cgi-bin/counter/counter.pl" alt="" width="65" height="20" /><br />
+Muddicts<br />
+and<br />
+counting</p>
+
+    </td>
+  </tr></table>
+
+<p><a href="http://validator.w3.org/"><img
+  src="/img/vxhtml10.gif"
+  alt="Valid XHTML 1.0!" height="31" width="88" border="0" /></a></p>
+
+  </td>
+</tr></table>
+</div>
+
+    </td>
+  </tr>
+
+  <tr>
+    <td bgcolor="#FFFFFF" width="15" valign="bottom"><img src="/img/clr.gif" alt="" width="1" height="1"
+      hspace="7" vspace="7" /></td>
+    <td bgcolor="#FFFFFF" width="410"><img src="/img/clr.gif" alt="" width="1" height="1" /></td>
+    <td bgcolor="#FFFFFF" width="15"><img src="/img/clr.gif" alt="" width="1" height="1" hspace="7" /></td>
+    <td bgcolor="#000000" width="140"><img src="/img/clr.gif" alt="" width="1" height="1" /></td>
+  </tr>
+
+  </table>
+
+  </td>
+  </tr>
+</table>
+
+
+</body>
+</html>
---
+++ poo.php
@@ -0,0 +1,94 @@
+echo '<?xml version="1.0" encoding="UTF-8" ?>';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+  <title>End of the Line LP Mud</title>
+  <meta name="keywords" content="End of the Line EotL LPmud LP Mud adventure game" />
+  <meta name="description" content="End of the Line, more commonly known as EotL, is a text-based roleplaying game in which you adventure in a myriad of landscapes and realms.  It is an LP-style of MUD that was first opened to the public in 1989." />
+  <link rel="STYLESHEET" type="text/css" href="/css/eotl_0.css" />
+</head>
+
+<body bgcolor="#000000" link="#996699" vlink="#9966CC">
+
+<table border="0" cellpadding="0" cellspacing="0">
+  <tr>
+  <td rowspan="20"><img src="/img/clr.gif" alt="" width="1" height="1" hspace="5" /></td>
+    <td bgcolor="#000000" width="582" nowrap="nowrap" align="center"><span style="color: #FFFFFF;"
+    ><img src="/img/title.gif"
+    alt="End of the Line LP Mud - 171.64.14.16 2010" width="582" height="90" /></span><br />
+    <table border="0" cellpadding="0" cellspacing="0"><tr><td height="1"></td></tr></table></td>
+  </tr>
+
+  <tr>
+    <td bgcolor="#660066"><table border="0" cellpadding="0" cellspacing="0"><tr><td height="1"></td></tr></table></td>
+  </tr>
+
+  <tr>
+    <td bgcolor="#000000"><table border="0" cellpadding="0" cellspacing="0"><tr><td height="9"></td></tr></table></td>
+  </tr>
+</table>
+
+<table border="0" cellpadding="1" cellspacing="0">
+<tr>
+  <td rowspan="20"><img src="/img/clr.gif" alt="" width="1" height="1" hspace="3" /></td>
+  <td bgcolor="#660066">
+
+  <table border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td bgcolor="#FFFFFF" width="15" height="15" valign="top"><img src="/img/clr.gif" alt="" width="1" height="1"
+      hspace="7" vspace="7" /></td>
+    <td bgcolor="#FFFFFF" width="410" height="15"><img src="/img/clr.gif" alt="" width="1" height="1" hspace="204" /></td>
+    <td bgcolor="#FFFFFF" width="15" height="15"><img src="/img/clr.gif" alt="" width="1" height="1" hspace="7" /></td>
+    <td bgcolor="#000000" width="140" height="15"><img src="/img/clr.gif" alt="" width="1" height="1" /></td>
+  </tr>
+
+  <tr>
+    <td bgcolor="#FFFFFF" width="15"><img src="/img/clr.gif" alt="" width="1" height="1" hspace="7" /></td>
+
+<!--  BEGIN MAIN SECTION  -->
+    <td bgcolor="#FFFFFF" width="410" valign="top">
+
+<p class="mypre">
+re: hannah [Gandalf, Thu Feb  1 13:32:45] 75<br />
++------------------------------------------------+<br />
+players shouldn't have to look at a website to get that information.
+</p>
+
+
+<p>Since this website has been declared pointless, then nobody will miss it, neh?</p>
+
+    </td>
+<!--  END MAIN SECTION  -->
+
+    <td bgcolor="#FFFFFF" width="14"><img src="/img/clr.gif" alt="" width="1" height="1" hspace="6" /></td>
+
+    <td align="left" bgcolor="#000000" valign="top" width="140">
+
+    </td>
+  </tr></table>
+
+  </td>
+</tr></table>
+</div>
+
+    </td>
+  </tr>
+
+  <tr>
+    <td bgcolor="#FFFFFF" width="15" valign="bottom"><img src="/img/clr.gif" alt="" width="1" height="1"
+      hspace="7" vspace="7" /></td>
+    <td bgcolor="#FFFFFF" width="410"><img src="/img/clr.gif" alt="" width="1" height="1" /></td>
+    <td bgcolor="#FFFFFF" width="15"><img src="/img/clr.gif" alt="" width="1" height="1" hspace="7" /></td>
+    <td bgcolor="#000000" width="140"><img src="/img/clr.gif" alt="" width="1" height="1" /></td>
+  </tr>
+
+  </table>
+
+  </td>
+  </tr>
+</table>
+
+
+</body>
+</html>
